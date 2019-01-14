68b6db2a75a3e9ec731056b2ec8b68e4cb9ad4ac
davblckqueen <ing.dsrr@gmail.com>
2019-01-13 20:20:34 -0500

    BE09 Building the house.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoor.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -18,7 +19,9 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	auto Owner = GetOwner();
+	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
 	
 }
 
